5b9c44f7a4b92149862fa9dca3aa4ae719de823b
franciscoyaya <franciscoperez@ceitartessos.com>
2019-10-27 23:01:02 +0100

    Hola Marijose

---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public string Nombre;
+	public string Profesion;
+	public string Aficion;
+	public int Edad;
+	public int Hijos;
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Francisco Perez, es un alumno que estudia en Tartessos.Su aficion es el futbol y las series , tiene 21 años muy bien llevados, por fortuna no tiene hijos ");
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
